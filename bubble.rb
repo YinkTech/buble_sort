1d3c9c1afa562192713e23f9d2c6021f70c9f40d
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2021-09-15 17:31:44 +0100

    correcting my linters error

--- bubble.rb
+++ bubble.rb
@@ -1,16 +1,14 @@
 def bubble_sort(array)
-	n = array.length
-	loop do
-	swapped = false
-	(n-1).times do|i|
-		if array[i] > array[i+1]
-			array[i], array[i + 1]= array[i + 1], array[i]
-
-		swapped = true
-		end
-	end
-
-	break if not swapped
+  n = array.length
+  loop do
+  swapped = false
+  (n-1).times do|i|
+    if array[i] > array[i+1]
+    array[i], array[i + 1]= array[i + 1], array[i]
+    swapped = true
+    end
+  end
+  break if not swapped
 end
 array
 end
@@ -24,7 +22,6 @@ def bubble_sort_by(arr)
   (0...arr.length).each do |_i|
     (0...arr.length - 1).each do |j|
       next unless (yield arr[j], arr[j + 1]).positive?
-
       temp = arr[j]
       arr[j] = arr[j + 1]
       arr[j + 1] = temp
