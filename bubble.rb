29f611e46dd51cb66c826f5e81fd294767235744
Olayinka Bidemi Ayeni <layinka4dat@gmail.com>
2021-09-15 16:52:37 +0100

    second bubble sort by

--- bubble.rb
+++ bubble.rb
@@ -17,3 +17,25 @@ end
 
 a = [1, 4, 6, 2, 1, 9, 8, 3]
 p bubble_sort(a)
+
+
+def bubble_sort_by(arr)
+  swap_test = true
+  (0...arr.length).each do |_i|
+    (0...arr.length - 1).each do |j|
+      next unless (yield arr[j], arr[j + 1]).positive?
+
+      temp = arr[j]
+      arr[j] = arr[j + 1]
+      arr[j + 1] = temp
+      swap_test = false
+    end
+    break if swap_test
+  end
+  arr
+end
+
+bsort_by = bubble_sort_by(%w[hello hey hi here]) do |left, right|
+  left.length - right.length
+end
+p bsort_by
